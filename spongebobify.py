78fa378f5109a72787a5917cdc6b7a03fef65eaf
cschwirian <cschwirian@gmail.com>
2018-12-01 12:58:07 -0700

    Organized library and transitioned to command line.

--- spongebobify.py
+++ spongebobify.py
@@ -1,20 +1,33 @@
-STRING = "Makes text look like this."
+# ------------------------------------------------------------------------------
+# Imports ----------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
+import sys
+
+
+# ------------------------------------------------------------------------------
+# Functions --------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 def spongebobify( string ):
-
+    # Keeps track of how many characters have been changed.
     character_index = 0
+    # Keeps track of the current place in the string.
     actual_index = 0
+
     while( actual_index < len( string ) ):
-        if( not string[actual_index].isalpha() ):
-            pass
-        else:
+        if( string[actual_index].isalpha() ):
             string = change_character( string, character_index, actual_index )
             character_index += 1
+
         actual_index += 1
 
     return string
 
+
+# ------------------------------------------------------------------------------
+# Helper Functions -------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 def change_character( string, character_index, actual_index ):
     if( character_index % 2 ):
@@ -37,5 +50,18 @@ def upper_character( string, index ):
         return string[0:index] + string[index].upper() + string[index + 1::]
 
 
+# ------------------------------------------------------------------------------
+# Main -------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 if( __name__ == "__main__" ):
-    print( spongebobify( STRING ) )
+    arguments = sys.argv
+
+    if( len( arguments ) > 2 ):
+        print( "This function only takes 1 argument.")
+    elif( len( arguments ) == 1 ):
+        print( "Must pass in a string." )
+    elif( type( arguments[1] ) != type( "" ) ):
+        print( "Must enter a string." )
+    else:
+        print( spongebobify( arguments[1] ) )
